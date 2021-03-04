873ce87485ba3ad155ccca1e8db4fd155c2fdbb7
Vlad <svad1804@gmail.com>
2021-03-04 17:53:55 +0200

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,33 @@
+﻿
+var $ = function (id) { return document.getElementById(id); }
+
+$("one").addEventListener("click", function () {
+    $("map1").style.visibility = "visible";
+});
+$("two").addEventListener("click", function () {
+    $("map2").style.visibility = "visible";
+});
+$("three").addEventListener("click", function () {
+    $("map3").style.visibility = "visible";
+})
+$("four").addEventListener("click", function () {
+    $("map4").style.visibility = "visible";
+})
+$("five").addEventListener("click", function () {
+    $("map5").style.visibility = "visible";
+})
+$("six").addEventListener("click", function () {
+    $("map6").style.visibility = "visible";
+})
+$("seven").addEventListener("click", function () {
+    $("map7").style.visibility = "visible";
+})
+$("eight").addEventListener("click", function () {
+    $("map8").style.visibility = "visible";
+})
+$("nine").addEventListener("click", function () {
+    $("map9").style.visibility = "visible";
+})
+$("ten").addEventListener("click", function () {
+    $("map10").style.visibility = "visible";
+})
